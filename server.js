dd75bea56961ed751ca480e67853cd5ab9ed3e6d
patifc <patifc@gmail.com>
2020-02-27 16:36:30 +0000

    server runing and login page created

---
+++ server.js
@@ -0,0 +1,26 @@
+
+const express = require('express');
+const fs      = require('fs');
+const path    = require('path');
+
+
+const app     = express();
+app.use(express.static(path.join(__dirname, 'views')));
+
+const showIndexFile = function (request, response){
+    const html    = fs.readFileSync('./views/index.html', 'utf-8');
+    const headers = {
+        "Content-Type": "text/html"
+    };
+    response.writeHead(200, headers)
+
+    response.end(html);
+}
+
+app.get('/', showIndexFile);
+
+
+
+app.listen(3000, function(){
+    console.log('listening');
+});
